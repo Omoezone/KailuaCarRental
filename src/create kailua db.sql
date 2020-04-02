6697b7410fe081788b040920b4d47f4909da9ab7
jakobbnt <57390360+jakobbnt@users.noreply.github.com>
2020-04-02 12:49:58 +0200

    Update create kailua db.sql

--- src/create kailua db.sql
+++ src/create kailua db.sql
@@ -1,4 +1,4 @@
-/*DROP DATABASE IF EXISTS kailua;
+DROP DATABASE IF EXISTS kailua;
 
 CREATE DATABASE kailua;
 
@@ -68,15 +68,23 @@ CREATE TABLE contracts
 );
 
 INSERT INTO zips VALUES 
-	(2200,'Nørrebro');
-
+	(2200,'Nørrebro'),
+    (2000, 'Frederiksberg'),
+    (2100,'Østerbro');
+    
 INSERT INTO customers VALUES
-	(1,'Hans','Hansen','Nørrebrogade 45','2983989','83982021',null,'Hans@Hansen.dk','1960-02-12',2200);
+	(1,'Hans','Hansen','Nørrebrogade 45','2983989','83982021',null,'Hans@Hansen.dk','1960-02-12',2200),
+    (2,'Jørgen','Andersen','Borgmester Fischers vej 15',' 2992093','27365481','31031929','j_jørgen@hotmail.com','1975-05-25',2000),
+    (3,'Mette','Hjortberg','Østerbrogade 47','8908987', '75679065',null,'Mette89@gmail.com','1996-07-30',2100);
     
 INSERT INTO cars VALUES 
-	('AT28938','Sport','Porsche','911 2017',1,0,370,'leather',2,1,4921,'benzin','2020-03-30',0);
+	('AT28938','Sport','Porsche','911 2017',1,0,370,'leather',2,1,4921,'benzin','2020-03-30',0),
+    ('DC29309','Luxury','Mercedes Benz','C-class',1,1,250,'leather','5','1',2900,'Benzin','2011-11-25',100),
+    ('PO20658','Family','Volvo','V90',1,0,210,'polyester',7,1,2900,'disel','2018-11-07',250);
     
 INSERT INTO contracts VALUES
-	(1,1,'2020-04-14','2020-03-31',4000,'AT28938');*/
+	(1,1,'2020-04-14','2020-03-31',4000,'AT28938'),
+    (2,3,'2019-09-20','2019-9-27',5000,'PO20658'),
+    (3,2,'2019-02-19','2019-2-26',3000,'DC29309');
     
 SET GLOBAL time_zone = '+1:00';
