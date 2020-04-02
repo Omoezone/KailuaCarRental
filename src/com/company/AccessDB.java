b78f74e50aa640d8e936421c12fa3ade7b99062b
Omoezone <30931803+Omoezone@users.noreply.github.com>
2020-04-02 13:01:39 +0200

    PrintF
    Signed-off-by: Omoezone <30931803+Omoezone@users.noreply.github.com>

--- src/com/company/AccessDB.java
+++ src/com/company/AccessDB.java
@@ -30,7 +30,7 @@ public class AccessDB{
            */
 
             // in the url we have to tell which account and password to use
-            con =  DriverManager.getConnection(DATABASE_URL,"root","Ecology15110515"); //DriverManager Class fra linje 186
+            con =  DriverManager.getConnection(DATABASE_URL,"root","PAS"); //DriverManager Class fra linje 186
 
             //*** now that the connection is established we do the query
             s = con.createStatement(); //Connection interface linje 105.
@@ -44,19 +44,19 @@ public class AccessDB{
             if (rs != null)
                 while (rs.next()) {
 
-                    System.out.printf("Data from name: %-10s\n",rs.getString("car_reg_number"));
-                    System.out.printf("Car model: %-10s\n",rs.getString("car_model"));
+                    System.out.printf("Data from name: %-14s",rs.getString("car_reg_number"));
+                    System.out.printf("Car model: %-16s",rs.getString("car_model"));
                     System.out.printf("Car Type: %-10s\n",rs.getString("car_type"));
-                    System.out.printf("Car cruise-control: %-10s\n",rs.getString("car_cruise_control"));
-                    System.out.printf("Car automatgear: %-10s\n",rs.getString("car_auto_gear"));
+                    System.out.printf("Car cruise-control: %-10s",rs.getString("car_cruise_control"));
+                    System.out.printf("Car automatgear: %-10s",rs.getString("car_auto_gear"));
                     System.out.printf("Car hp: %-10s\n",rs.getString("car_hp"));
-                    System.out.printf("Car seat material: %-10s\n",rs.getString("car_seat_material"));
-                    System.out.printf("Car seat numbers: %-10s\n",rs.getString("car_seat_number"));
+                    System.out.printf("Car seat material: %-11s",rs.getString("car_seat_material"));
+                    System.out.printf("Car seat numbers: %-9s",rs.getString("car_seat_number"));
                     System.out.printf("Car ac: %-10s\n",rs.getString("car_ac"));
-                    System.out.printf("Car ccm: %-10s\n",rs.getString("car_ccm"));
-                    System.out.printf("Car fuel type: %-10s\n",rs.getString("car_fuel_type"));
-                    System.out.printf("Car reg date: %-10s\n",rs.getString("car_reg_date"));
-                    System.out.printf("Car odometer: %-10s\n",rs.getString("car_odometer"));
+                    System.out.printf("Car ccm: %-21s",rs.getString("car_ccm"));
+                    System.out.printf("Car fuel type: %-12s",rs.getString("car_fuel_type"));
+                    System.out.printf("Car reg date: %-15s",rs.getString("car_reg_date"));
+                    System.out.printf("Car odometer: %-10s\n\n",rs.getString("car_odometer"));
 
                 }
             s.close();
