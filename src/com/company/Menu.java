afb94c97687a289e340e932cd55b89f59cbae8a2
jakobbnt <57390360+jakobbnt@users.noreply.github.com>
2020-04-06 10:18:11 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/Omoezone/KailuaCarRental""
    This reverts commit 9a94092dcd822657feaebf9b677117033f265f42.

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -1,10 +1,6 @@
 package com.company;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet; //Interface
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.lang.*;
@@ -20,13 +16,17 @@ public class Menu {
              con = null;
              Statement s = null;
              Class.forName(JDBC_DRIVER);
-             con = DriverManager.getConnection(DATABASE_URL, "root", "X7913bz1h11");
+             con = DriverManager.getConnection(DATABASE_URL, "root", "Omoezone12");
              s = con.createStatement();
 
-
+             //ArrayLists til at indeholde midlertidlig data
+             ArrayList<String> cusList = new ArrayList<>();
+             ArrayList<String> carList = new ArrayList<>();
+             ArrayList<String> conList = new ArrayList<>();
+             ArrayList<String> zipList = new ArrayList<>();
              //loops while user inputs y
              boolean mainMenu = true;
-             while (mainMenu == true) {
+             while (mainMenu) {
                  //Choose which object to create 1,2 or 3
                  System.out.println("Press 1 to create new DB entry \npress 2 to change an existing entry \npress 3 to remove an existing entry \npress 4 to print list of entries");
                  int choice = console.nextInt();
@@ -34,52 +34,173 @@ public class Menu {
                      case 1:
                          System.out.println("Press 1 for new customer \nPress 2 for new contract \nPress 3 for new car \nPress 4 for new city");
                          int choiceCreate = console.nextInt();
-                         ArrayList<String> objectCreation = new ArrayList<>();
+                         // ArrayList<String> objectCreation = new ArrayList<>(); //Temp ArrayList der indeholder de informationer vi bruger til at sende excecuteUpdate statements til DB
                          switch (choiceCreate) {
                              case 1:
-                                 String[] customerPrompts = {"Input the following information: ","First name?", "Last name?", "Address?", "Zip code?", "Mobile number?", "Phone?", "Email?", "When did the driver start driving?"};
-                                 for (int i = 1; i < 10; i++) {
+                                 String[] customerPrompts = {"Input the following information: ","First name?", "Last name?", "Address?", "license Number", "Mobile number?", "Phone?", "Email?", "When did the driver start driving?","zip code","City"};
+                                 for (int i = 1; i < 11; i++) {
                                      System.out.println(customerPrompts[0]);
                                      System.out.println(customerPrompts[i]);
-                                     objectCreation.add(console.next());
+                                     cusList.add(console.next());
                                  }
-                                 s.executeUpdate();
+                                 //Creates zips table
+                                 s.executeUpdate("INSERT INTO zips VALUES('"+Integer.parseInt(cusList.get(8))+"','"+cusList.get(9)+"')");
+                                 int i = 4; //TODO fix så det er auto increment måske?
+                                 // Creates customers table data
+                                 s.executeUpdate("INSERT INTO customers VALUES ('"+i+"','"+ cusList.get(0)+"','"+cusList.get(1)+"','"+cusList.get(2)+"','"+cusList.get(3)+"','"+cusList.get(4)+"','"+cusList.get(5)+"','"+cusList.get(6)+"','"+cusList.get(7)+"','"+Integer.parseInt(cusList.get(8))+"')");
                                  break;
                              case 2:
-
+                                //TODO Create contract (Igang)
+                                 /* Nødvendige FRIE data vi behøver for at kunne lave en contract er:
+                                    1. customer_id fra customer
+                                    2. car_reg_number fra cars
+                                    Altså skal der både være lavet et customers og cars data sæt, der ikke er koblet op på noget, for at vi vil kunne lave en kontrakt.
+                                 *  */
+                                 String[] contractPrompt = {"Input the following information","contract_id","customer_id","contract_to_date","contract_from_date","contract_max_km","car_reg_number"};
+                                 for (int h = 1; h < contractPrompt.length; h++) {
+                                     System.out.println(0);
+                                     System.out.println(h);
+                                     conList.add(console.next());
+                                 }
+                                 // Tænker at vi kan tage customer_id fra cusList og car_reg_number fra carList
+                                 s.executeUpdate("INSERT INTO contracts VALUES('"+conList.get(1)+"','"+conList.get(2)+"','"+conList.get(3)+"','"+conList.get(4)+"','"+conList.get(5)+"','"+conList.get(6)+"')");
                                  break;
                              case 3:
-
+                                 String[] carPrompts = {"Input the following information for the car: ","Registration number","Type","Brand","Model","cruise control","automatic gear?","Horse Power","Seat Material","Number of seats","Air Condition?","CCM","Fuel Type","Registration date","Odometer"};
+                                 for (int j = 1; j < carPrompts.length; j++) {
+                                     System.out.println(carPrompts[0]);
+                                     System.out.println(carPrompts[j]);
+                                     carList.add(console.next());
+                                 }
+                                 // Det næste statement tager information der er blivet besvaret i sout prompt i forloopet foroven, og sætter det ind i et sql statement, der matcher de forventede værdier.
+                                 s.executeUpdate("INSERT INTO cars VALUES ('"+carList.get(0)+"','"+carList.get(1)+"','"+carList.get(2)+"','"+carList.get(3)+"','"+Integer.parseInt(carList.get(4))+"','"+Integer.parseInt(carList.get(5))+"','"+
+                                         Integer.parseInt(carList.get(6))+"','"+carList.get(7)+"','"+Integer.parseInt(carList.get(8))+"','"+Integer.parseInt(carList.get(9))+"','"+Integer.parseInt(carList.get(10))+"','"+carList.get(11)+"','"+carList.get(12)+"','"+Integer.parseInt(carList.get(13))+"')");
                                  break;
-                             case 4:
-
+                             case 4: //Til at lave en seperat zip kode uden en customer. De bliver lavet sammen med customer i case 1
+                                 String[] zipPrompts = {"Input the following information for the car: ","zip code","city"};
+                                 for (int k = 1; k < zipPrompts.length; k++) {
+                                     System.out.println(zipPrompts[0]);
+                                     System.out.println(zipPrompts[k]);
+                                     zipList.add(console.next());
+                                 }
+                                 s.executeUpdate("INSERT INTO zips VALUES ('"+zipList.get(0)+"','"+zipList.get(1)+"')");
                                  break;
-
                          }
                          break;
                      case 2:
-                         //changeObject(console, cars, renters, contracts);
+                         //TODO updateObject;
+                         System.out.println("What information do you wish to change?\n1.Customer\n2.Contract\n3.Car\n4.zips");
                          break;
                      case 3:
-                         //removeObject(console, cars, renters, contracts);
+                         // TODO removeObject; (Behøver vi dette, eller kan vi ikke bare nøjes med at fokusere på update?)
+                         System.out.println("What information do you want to remove?\n1.Customer\n2.Contract\n3.Car\n4.zips");
+                         int removeChoice = console.nextInt();
+                         switch (removeChoice){
+                             case 1:
+
+                                 break;
+                             case 2:
+                                 break;
+                             case 3:
+                                 break;
+                             case 4:
+                                 /*System.out.println("Which zip do you want to delete? (Input the zip code)");
+                                 ResultSet rsCu = s.executeQuery("SELECT customer_id,customer_first_name,customer_last_name,customer_address,customer_license_number, " +
+                                         "customer_mobile_phone,customer_phone,customer_email,customer_driver_since_date,zip_code FROM customers");
+                                 if(rsCu != null)
+                                     while(rsCu.next()){
+                                         System.out.printf("zipcode: %s\n",rsCu.getString("zip_code"));
+                                     }
+                                 int zipDelete = console.nextInt();
+                                     s.executeUpdate("SET FOREIGN_KEY_CHECKS=0; ");
+                                 s.executeUpdate("DELETE FROM zips WHERE zip_code='"+zipDelete+"'");
+                                 s.executeUpdate("UPDATE customers SET zip_code = '"+(zipDelete-1)+"' WHERE zip_code = '"+zipDelete+"'");
+                                 s.executeUpdate("SET FOREIGN_KEY_CHECKS=1; "); */
+                                 break;
+                         }
                          break;
-                     case 4:
-                         //printObjects(cars, renters, contracts);
+                     case 4: // Hver case i denne del, består af et excecuteQuery der ved hjælp af printF statements, udprinter en given table
+                         System.out.println("Which information do you want to print?\n1.Customers\n2.Cars\n3.Contracts\n4.Cities");
+                         int printChoise = console.nextInt();
+                         switch (printChoise){
+                             case 1: // Udprintning af customers table
+                                 ResultSet rsCu = s.executeQuery("SELECT customer_id,customer_first_name,customer_last_name,customer_address,customer_license_number, " +
+                                         "customer_mobile_phone,customer_phone,customer_email,customer_driver_since_date,zip_code FROM customers");
+                                 if(rsCu != null) {
+                                     while (rsCu.next()) {
+                                         System.out.printf("customer id: %-32s", rsCu.getString("customer_id"));
+                                         System.out.printf("customer name: %s %-23s", rsCu.getString("customer_first_name"), rsCu.getString("customer_last_name"));
+                                         System.out.printf("customer address: %-10s\n", rsCu.getString("customer_address"));
+                                         System.out.printf("customer license number: %-20s", rsCu.getString("customer_license_number"));
+                                         System.out.printf("customer mobile phone: %-20s", rsCu.getString("customer_mobile_phone"));
+                                         System.out.printf("customer phone: %-10s\n", rsCu.getString("customer_phone"));
+                                         System.out.printf("customer email: %-29s", rsCu.getString("customer_email"));
+                                         System.out.printf("customer driver since: %-20s", rsCu.getString("customer_driver_since_date"));
+                                         System.out.printf("customer zipcode: %-10s\n\n", rsCu.getString("zip_code"));
+                                     }
+                                 }
+                                 break;
+                             case 2: // Udprintning af cars table
+                                 ResultSet rsCar = s.executeQuery("SELECT car_reg_number,car_type,car_brand,car_model,car_cruise_control,car_auto_gear,car_hp," +
+                                         "car_seat_material,car_seat_number,car_ac,car_ccm,car_fuel_type,car_reg_date,car_odometer FROM cars");
+                                 if(rsCar != null){
+                                     while(rsCar.next()){
+                                         System.out.printf("Car reg number: %-14s",rsCar.getString("car_reg_number"));
+                                         System.out.printf("Car type: %-17s",rsCar.getString("car_type"));
+                                         System.out.printf("Car brand & model: %s %-10s\n",rsCar.getString("car_brand"),rsCar.getString("car_model"));
+                                         System.out.printf("Car cruise-control: %-10s",rsCar.getString("car_cruise_control"));
+                                         System.out.printf("Car auto-gear: %-12s",rsCar.getString("car_auto_gear"));
+                                         System.out.printf("Car hp: %-10s\n",rsCar.getString("car_hp"));
+                                         System.out.printf("Car seat material: %-11s",rsCar.getString("car_seat_material"));
+                                         System.out.printf("Car seat numbers: %-9s",rsCar.getString("car_seat_number"));
+                                         System.out.printf("Car ac: %-10s\n",rsCar.getString("car_ac"));
+                                         System.out.printf("Car ccm: %-21s",rsCar.getString("car_ccm"));
+                                         System.out.printf("Car fuel type: %-12s",rsCar.getString("car_fuel_type"));
+                                         System.out.printf("Car reg date: %-15s",rsCar.getString("car_reg_date"));
+                                         System.out.printf("Car odometer: %-10s\n\n",rsCar.getString("car_odometer"));
+                                     }
+                                 }
+                                 break;
+                             case 3: //Prints the contracts table
+                                 ResultSet rsCon = s.executeQuery("SELECT contract_id,customer_id,contract_to_date,contract_from_date,contract_max_km,car_reg_number FROM contracts");
+                                 if(rsCon != null){
+                                     while(rsCon.next()){
+                                         System.out.printf("Contract id: %-16s",rsCon.getString("contract_id"));
+                                         System.out.printf("Customer id: %-17s",rsCon.getString("customer_id"));
+                                         System.out.printf("Contract period: From '%s' to '%-10s'\n",rsCon.getString("contract_from_date"),rsCon.getString("contract_to_date"));
+                                         System.out.printf("Contract max km: %-12s",rsCon.getString("contract_max_km"));
+                                         System.out.printf("Car number: %-10s\n\n",rsCon.getString("car_reg_number"));
+                                     }
+                                 }
+                                 break;
+                             case 4: //Prints the zips table
+                                 ResultSet rsZip = s.executeQuery("SELECT zip_code,zip_city FROM zips");
+                                 if(rsZip != null){
+                                     while(rsZip.next()){
+                                         System.out.printf("Zip code: %-12s",rsZip.getString("zip_code"));
+                                         System.out.printf("City for zip code: %s\n\n",rsZip.getString("zip_city"));
+                                     }
+                                 }
+                                 break;
+                         }
                          break;
                  }
 
-                 console.nextLine();
+                 console.nextLine(); //TODO hvad gør denne?
                  System.out.println("Return to main menu? y/n");
                  String answer = console.nextLine();
-                 if (answer.equals("n")) {
+                 if (answer.equalsIgnoreCase("n")) {
+                     System.exit(1);
                      mainMenu = false;
                  }
              }
              /* Errorhandling */
+             s.close(); //Lukker statement objektet
+             con.close(); //Lukker DriverManger (Forbindelsen til DB)
          }
-             catch (SQLException sqlex) {
+         catch(SQLException sqlex) {
             try{
-                System.out.println(sqlex.getMessage());
+                System.out.println(sqlex.getMessage()); //TODO Ved vi hvad getMessage() findes ?
                 con.close();
                 System.exit(1);  // terminate program
             }
